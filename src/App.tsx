d439ca63e688546c0c1a2f003c345e932a2497a3
irembuz <irembuz7@gmail.com>
2022-04-21 10:16:10 +0300

    npm install dayjs

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import React from "react";
-//import "./App.css";
+import "./App.css";
 import Header from "./components/Header";
 import SideBar from "./components/SideBar";
 import HomeScreen from "./screens/HomeScreen";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <Header />
-      <SideBar />
-      <HomeScreen />
-    </div>
+    <React.Fragment>
+      <div>
+        <Header />
+        <div>
+          <SideBar />
+          <HomeScreen />
+        </div>
+      </div>
+    </React.Fragment>
   );
-}
+};
 
 export default App;
