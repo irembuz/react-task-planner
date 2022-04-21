725a451512a7a4f2a66df92f4ddc29fa818aafea
irembuz <irembuz7@gmail.com>
2022-04-21 16:33:25 +0300

    create tables of the day

--- src/components/Day.tsx
+++ src/components/Day.tsx
@@ -1,7 +1,35 @@
-import React from "react";
+import { listenerCount } from "process";
+import React, { useState } from "react";
+
+const INITIAL_STATE = [
+  { id: 1, time: "8.30", task: "Kitap oku" },
+  { id: 2, time: "12.30", task: "Kedileri besle" },
+];
 
 const Day = () => {
-  return <div>Day</div>;
+  const [tasks, setTasks] = useState(INITIAL_STATE);
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th></th>
+            <th>Time</th>
+            <th>Task</th>
+          </tr>
+        </thead>
+        <tbody className="task">
+          {tasks.map((item) => (
+            <tr>
+              <td>{item.time}</td>
+              <td>{item.task}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default Day;
