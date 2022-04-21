8f5e0afd3c22b178b82af7bad5917553a7fae998
irembuz <irembuz7@gmail.com>
2022-04-21 09:54:09 +0300

    base components are created

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+//import "./App.css";
+import Header from "./components/Header";
+import SideBar from "./components/SideBar";
+import HomeScreen from "./screens/HomeScreen";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <SideBar />
+      <HomeScreen />
     </div>
   );
 }
