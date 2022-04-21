1b7757f22202ef2fa44b4460186b5af92e1fdbcf
irembuz <irembuz7@gmail.com>
2022-04-21 21:36:37 +0300

    Header initial design

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,21 @@
 import React from "react";
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
 
-const Header = () => {
+export default function Header() {
   return (
-    <header>
-      Header
+    <header className="px-4 py-2 flex items-center">
+      <h1 className="mr-10 text-xl text-gray-600 fond-bold">Task Planner</h1>
+      <button className="border rounded py-2 px-4 mr-5">Today</button>
+      <button>
+        <span className="cursor-pointer text-gray-600 mx-2">
+          <MdKeyboardArrowLeft />
+        </span>
+      </button>
+      <button>
+        <span className="cursor-pointer text-gray-600 mx-2">
+          <MdKeyboardArrowRight />
+        </span>
+      </button>
     </header>
   );
-};
-
-export default Header;
+}
